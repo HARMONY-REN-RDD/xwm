8caf905c76e35cd38de87adbbed2f6205a4070b1
kai5041 <kai5041contact@gmail.com>
2025-08-09 13:44:44 +0200

    install_sdk.sh test

--- include/xuci/core.hpp
+++ include/xuci/core.hpp
@@ -3,4 +3,6 @@
 #include <xuci/types/memory.hpp>
 #include <xuci/types/register.hpp>
 
+#include <xuci/base_architecture.hpp>
+
 namespace xuci {}
--- include/xuci/types/register.hpp
+++ include/xuci/types/register.hpp
@@ -14,15 +14,58 @@ public:
 
   Register() = default;
 
-  std::string to_hex() {
+  std::string to_hex(bool little = true) const {
     std::ostringstream oss;
-
-    for (auto i = 0; i < this->n_bytes; i++) {
-      oss << std::hex << std::setw(2) << std::setfill('0')
-          << static_cast<int>(this->read(i));
+    if (little) {
+      for (int i = 0; i < n_bytes; ++i) {
+        oss << std::hex << std::setw(2) << std::setfill('0')
+            << static_cast<int>(this->read(i));
+      }
+    } else {
+      for (int i = n_bytes - 1; i >= 0; --i) {
+        oss << std::hex << std::setw(2) << std::setfill('0')
+            << static_cast<int>(this->read(i));
+      }
     }
-
     return oss.str();
+  }
+
+  std::uint64_t to_unsigned(bool little = true) {
+    uint64_t result = 0;
+    if (little) {
+      for (int i = 0; i < n_bytes; ++i) {
+        result |= static_cast<uint64_t>(this->read(i)) << (8 * i);
+      }
+    } else { // Big endian
+      for (int i = 0; i < n_bytes; ++i) {
+        result = (result << 8) | this->read(i);
+      }
+    }
+    return result;
+  }
+
+  void increment() {
+    for (int i = 0; i < n_bytes; ++i) {
+      uint8_t val = this->read(i);
+      if (val == 0xFF) {
+        this->write(i, 0x00);
+      } else {
+        this->write(i, val + 1);
+        break;
+      }
+    }
+  }
+
+  void decrement() {
+    for (int i = 0; i < n_bytes; ++i) {
+      uint8_t val = this->read(i);
+      if (val == 0x00) {
+        this->write(i, 0xFF);
+      } else {
+        this->write(i, val - 1);
+        break;
+      }
+    }
   }
 };
 
