cfd1de2d03e033cd24a186129e41851a5924a255
kai5041 <kai5041contact@gmail.com>
2025-08-16 19:22:15 +0200

    Dumb mistake

--- instal_sdk.sh
+++ instal_sdk.sh
@@ -3,13 +3,8 @@ set -e
 
 rm -rf include/xuci
 git clone https://github.com/HARMONY-REN-RDD/xuci.git
-cd xuci
-./build.sh linux
-./build.sh windows
-cd ..
 cp -r ./xuci/include ./
 mkdir -p objs
 cp xuci/src/base_architecture.cpp ./src
 cp xuci/build/*/base_architecture.* ./objs
 rm -rf xuci
-
