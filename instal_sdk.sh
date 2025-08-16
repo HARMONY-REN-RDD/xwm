749c338117420a10a3de6388195621ca969bbb57
kai5041 <kai5041contact@gmail.com>
2025-08-16 19:24:58 +0200

    Minimize

--- instal_sdk.sh
+++ instal_sdk.sh
@@ -6,5 +6,4 @@ git clone https://github.com/HARMONY-REN-RDD/xuci.git
 cp -r ./xuci/include ./
 mkdir -p objs
 cp xuci/src/base_architecture.cpp ./src
-cp xuci/build/*/base_architecture.* ./objs
 rm -rf xuci
